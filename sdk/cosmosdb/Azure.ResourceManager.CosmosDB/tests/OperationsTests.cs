1982d361d9d8d9bbce7b23664773e7131f49951f
nisha-bhatia <Nisha.Bhatia@microsoft.com>
2020-08-20 13:59:42 -0700

    Update OperationsTests.cs
    clean up the code

--- sdk/cosmosdb/Azure.ResourceManager.CosmosDB/tests/OperationsTests.cs
+++ sdk/cosmosdb/Azure.ResourceManager.CosmosDB/tests/OperationsTests.cs
@@ -1,17 +1,9 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-
 using NUnit.Framework;
 using System.Threading.Tasks;
-using Azure.Management.Resources;
-using Azure.Management.Resources.Models;
-using System;
 using CosmosDB.Tests;
-using System.Net;
-using Moq;
-using System.Runtime.CompilerServices;
-using Azure.ResourceManager.CosmosDB.Models;
 using Azure.Core.TestFramework;
 
 namespace Azure.ResourceManager.CosmosDB.Tests
@@ -20,7 +12,7 @@ namespace Azure.ResourceManager.CosmosDB.Tests
     public class OperationsTests : CosmosDBManagementClientBase
     {
         public string resourceName;
-        public OperationsTests() : base(true)
+        public OperationsTests(): base(true)
         {
             resourceName = "sample689323531";
         }
@@ -49,18 +41,5 @@ namespace Azure.ResourceManager.CosmosDB.Tests
             Assert.NotNull(operations);
             Assert.IsNotEmpty(await operations.ToEnumerableAsync());
         }
-        //[TestCase]
-        //public async Task SampleTest()
-        //{
-        //    var ResourcesOperations = ResourcesManagementClient.ResourceGroups;
-        //    await TryRegisterResourceGroupAsync(ResourcesOperations, "West US", "sample4");
-        //    Console.WriteLine("past await");
-        //}
-        //public static async Task<ResourceGroup> TryRegisterResourceGroupAsync(ResourceGroupsOperations resourceGroupsOperations, string location, string resourceGroupName)
-        //{
-        //    var createResponse = await resourceGroupsOperations.CreateOrUpdateAsync(resourceGroupName, new ResourceGroup(location));
-        //    Assert.IsNotNull(createResponse);
-        //    return createResponse.Value;
-        //}
     }
 }
