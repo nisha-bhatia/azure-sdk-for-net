5bd1ca0231b70fd4e310171e7f2b07233b2c5b64
Nisha Bhatia <67986960+nisha-bhatia@users.noreply.github.com>
2022-11-01 13:27:54 -0700

    Update ConcurrencyTest.cs

--- sdk/monitor/Azure.Monitor.Ingestion/tests/ConcurrencyTest.cs
+++ sdk/monitor/Azure.Monitor.Ingestion/tests/ConcurrencyTest.cs
@@ -18,6 +18,7 @@ namespace Azure.Monitor.Ingestion.Tests
     public class ConcurrencyTest : RecordedTestBase<MonitorIngestionTestEnvironment>
     {
         private const int Mb = 1024 * 1024;
+        private List<object> entryList;
         public ConcurrencyTest(bool isAsync) : base(isAsync, RecordedTestMode.Live)
         {
         }
@@ -33,6 +34,8 @@ namespace Azure.Monitor.Ingestion.Tests
                 options.AddPolicy(policy, HttpPipelinePosition.PerCall);
             }
             var clientOptions = InstrumentClientOptions(options);
+            //entryList = GenerateEntries(290000, Recording.Now.DateTime);
+            entryList = GenerateEntries(50000, Recording.Now.DateTime);
             return InstrumentClient(new LogsIngestionClient(new Uri(TestEnvironment.DCREndpoint), TestEnvironment.Credential, clientOptions));
         }
 
@@ -52,23 +55,106 @@ namespace Azure.Monitor.Ingestion.Tests
             return entries;
         }
 
+        [AsyncOnly]
         [Test]
         public async Task Concurrency5()
         {
             LogsIngestionClient client = CreateClient();
 
-            var x = GenerateEntries(1000000, Recording.Now.DateTime);
-            long y;
-            var entry = BinaryData.FromObjectAsJson(x);
-            y = entry.ToMemory().Length;
-            bool pass = false;
-            if (y >= 2000000000)
-            {
-                pass = true;
-            }
-            Console.WriteLine(pass);
+            //var x = GenerateEntries(220000, Recording.Now.DateTime);
+            //long y;
+            //var entry = BinaryData.FromObjectAsJson(x);
+            //y = entry.ToMemory().Length;
+            //bool pass1 = false;
+            //bool pass15 = false;
+            //bool pass2 = false;
+            //if (y >= 1000000000)
+            //{
+            //    pass1 = true;
+            //}
+            //if (y >= 1500000000)
+            //{
+            //    pass15 = true;
+            //}
+            //if (y >= 2000000000)
+            //{
+            //    pass2 = true;
+            //}
+            //Console.WriteLine(pass1);
+            //Console.WriteLine(pass15);
+            //Console.WriteLine(pass2);
             // Make a 2 Gb request
-            var response = await client.UploadAsync(TestEnvironment.DCRImmutableId, TestEnvironment.StreamName, GenerateEntries(1000, Recording.Now.DateTime)).ConfigureAwait(false);
+            var response = await client.UploadAsync(TestEnvironment.DCRImmutableId, TestEnvironment.StreamName, entryList).ConfigureAwait(false);
+
+            // Check the response
+            Assert.AreEqual(UploadLogsStatus.Success, response.Value.Status);
+            Assert.IsEmpty(response.Value.Errors);
+        }
+
+        [AsyncOnly]
+        [Test]
+        public async Task Concurrency100()
+        {
+            LogsIngestionClient client = CreateClient();
+            UploadLogsOptions options = new UploadLogsOptions();
+            options.MaxConcurrency = 100;
+            var response = await client.UploadAsync(TestEnvironment.DCRImmutableId, TestEnvironment.StreamName, entryList).ConfigureAwait(false);
+
+            // Check the response
+            Assert.AreEqual(UploadLogsStatus.Success, response.Value.Status);
+            Assert.IsEmpty(response.Value.Errors);
+        }
+
+        [AsyncOnly]
+        [Test]
+        public async Task Concurrency1000()
+        {
+            LogsIngestionClient client = CreateClient();
+            UploadLogsOptions options = new UploadLogsOptions();
+            options.MaxConcurrency = 1000;
+            var response = await client.UploadAsync(TestEnvironment.DCRImmutableId, TestEnvironment.StreamName, entryList).ConfigureAwait(false);
+
+            // Check the response
+            Assert.AreEqual(UploadLogsStatus.Success, response.Value.Status);
+            Assert.IsEmpty(response.Value.Errors);
+        }
+
+        [AsyncOnly]
+        [Test]
+        public async Task Concurrency5000()
+        {
+            LogsIngestionClient client = CreateClient();
+            UploadLogsOptions options = new UploadLogsOptions();
+            options.MaxConcurrency = 5000;
+            var response = await client.UploadAsync(TestEnvironment.DCRImmutableId, TestEnvironment.StreamName, entryList).ConfigureAwait(false);
+
+            // Check the response
+            Assert.AreEqual(UploadLogsStatus.Success, response.Value.Status);
+            Assert.IsEmpty(response.Value.Errors);
+        }
+
+        [AsyncOnly]
+        [Test]
+        public async Task Concurrency10000()
+        {
+            LogsIngestionClient client = CreateClient();
+            UploadLogsOptions options = new UploadLogsOptions();
+            options.MaxConcurrency = 10000;
+            var response = await client.UploadAsync(TestEnvironment.DCRImmutableId, TestEnvironment.StreamName, entryList).ConfigureAwait(false);
+
+            // Check the response
+            Assert.AreEqual(UploadLogsStatus.Success, response.Value.Status);
+            Assert.IsEmpty(response.Value.Errors);
+        }
+
+        [AsyncOnly]
+        [Test]
+        public async Task Concurrency500000()
+        {
+            LogsIngestionClient client = CreateClient();
+            UploadLogsOptions options = new UploadLogsOptions();
+            options.MaxConcurrency = 500000;
+            var response = await client.UploadAsync(TestEnvironment.DCRImmutableId, TestEnvironment.StreamName, entryList).ConfigureAwait(false);
 
             // Check the response
             Assert.AreEqual(UploadLogsStatus.Success, response.Value.Status);
